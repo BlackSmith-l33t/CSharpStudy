e2f2b83746a6784c3e208f5eb28408f6ee0e7f8a
tranquila <sdf383000@gmail.com>
2021-08-19 08:58:24 +0900

    Property

---
+++ Propety/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Propety
+{
+    class Program
+    {
+        // 객체지향 -> 은닉성
+        class Knight
+        {
+            protected int hp;
+
+            public int Hp // int Hp { get; set; } = 100; 
+            {
+                get { return hp; }  
+                set { hp = value; }
+
+                //get; 
+                //set;               
+            } 
+        }
+
+        static void Main(string[] args)
+        {
+            // 프로퍼티
+            Knight knight = new Knight();
+            knight.Hp = 100;
+
+            int hp = knight.Hp;
+        }
+    }
+}
