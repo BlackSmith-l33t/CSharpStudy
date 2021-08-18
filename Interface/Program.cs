2286dc35d6720a6c83afa293754aef0f9ff0f87d
tranquila <sdf383000@gmail.com>
2021-08-18 12:01:16 +0900

    Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        abstract class Monster // 추상클래스
+        {
+            public abstract void Shout();
+        } 
+
+        interface IFlyable // 인터페이스
+        {
+            void Fly();
+        }
+
+        class Orc : Monster
+        {
+            public override void Shout()
+            {
+                Console.WriteLine("록타를 오가르!");
+            }
+        }
+
+        class FlyableOrc : Orc, IFlyable
+        {
+            public void Fly()
+            {
+
+            }
+        }
+
+        class Skeleton : Monster
+        {
+            public override void Shout()
+            {
+                Console.WriteLine("꾸엑!");
+            }
+        }
+         
+        static void DoFly(IFlyable flyable)
+        {
+            flyable.Fly();
+        }
+
+        static void Main(string[] args)
+        {
+            IFlyable orc = new FlyableOrc();
+            DoFly(orc);
+        }
+    }
+}
