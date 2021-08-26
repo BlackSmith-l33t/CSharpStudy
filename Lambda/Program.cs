ae824f1ca31bf089efa1bd3a6ec6c57ebedbcf44
Nobody <nobody133738@gmail.com>
2021-08-27 06:52:35 +0900

    Lambda(람다)

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -40,6 +40,20 @@ namespace Lambda
             return null;
         }
 
+        static Item FindItem2(MyFunc<Item, bool> selector)
+        {
+            foreach (Item item in _items)
+            {
+                if (selector(item))
+                    return item;
+            }
+            return null;
+        }
+
+        delegate Return MyFunc<Return>();
+        delegate Return MyFunc<T, Return>(T item);
+        delegate Return MyFunc<T1, T2, Return>(T1 t1, T2 t2);
+
         static void Main(string[] args)
         {
             _items.Add(new Item() { ItemType = ItemType.Weapon, Rarity = Rarity.Normal });
@@ -50,7 +64,20 @@ namespace Lambda
             Item item = FindItem((Item item) => { return item.ItemType == ItemType.Weapon; });
 
             //Anonymous Funtion : 무명 함수 / 익명 함수
-            Item item2 = FindItem(delegate (Item item) { return item.ItemType == ItemType.Weapon;});
+            Item item2 = FindItem(delegate (Item item) { return item.ItemType == ItemType.Weapon; });
+
+            ItemSelector selector = new ItemSelector((Item Item) => {return item.ItemType == ItemType.Weapon; });
+
+            Item item3 = FindItem(selector);
+
+            MyFunc<Item, bool> selector2 = (Item item) => { return item.ItemType == ItemType.Weapon; };
+
+            Item item4 = FindItem2(selector2);
+
+            // delegate를 직접 선언하지 않아도, 이미 만들어진 애들이 존재한다.
+            // -> 반환 타입이 있을 경우 Func ex) Func<Item, bool> selector = (Item item) => { return item.ItemType == itemType.Weapon; };
+            // -> 반화 타입이 없으면 Action
         }
     }
 }
+ 
