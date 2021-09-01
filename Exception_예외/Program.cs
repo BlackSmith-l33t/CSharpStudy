01d0dfad006cff81b4977b861b2d0c0730492b62
Nobody <nobody133738@gmail.com>
2021-09-01 21:12:27 +0900

    Exception(예외 처리)

---
+++ Exception_예외/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exception_예외
+{
+    class Program
+    {
+        class TestException : Exception
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                //1. 0으로 나눌때
+                //2. 잘못된 메모리를 참조(null)
+                //3. 오버플로우
+
+                throw new TestException();  
+            }
+            catch (DivideByZeroException e)
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                // DB 파일 정리 등등
+            }
+        }
+    }
+}
