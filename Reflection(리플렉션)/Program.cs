714995dba6a664f70b21e9e8afb838cba8575a76
Nobody <nobody133738@gmail.com>
2021-09-02 09:35:52 +0900

    Reflection & Attrubute

---
+++ Reflection(리플렉션)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Reflection_리플렉션_
+{
+    class Program
+    {
+        class Important : System.Attribute
+        {
+            string message;
+
+            public Important(string message) { this.message = message; }
+        }
+
+        class Monster
+        {
+            //  hp입니다. 중요한 정보입니다.
+            [Important("very Important")]
+            public int hp;
+            protected int attack;
+            private float speed;
+
+            void Attack() { }
+        }
+
+        static void Main(string[] args)
+        {
+            //Reflection : X - Ray
+            Monster monster = new Monster();
+            Type type = monster.GetType();
+
+            var fields = type.GetFields(System.Reflection.BindingFlags.Public
+            | System.Reflection.BindingFlags.NonPublic
+            | System.Reflection.BindingFlags.Static
+            | System.Reflection.BindingFlags.Instance);
+
+            foreach (FieldInfo field in fields)
+            {
+                string access = "protected";
+                if (field.IsPublic)
+                {
+                    access = "public";                  
+                }
+                else if (field.IsPrivate)
+                {
+                    access = "private";
+                }
+
+                var attributes = field.GetCustomAttributes();
+
+                Console.WriteLine($"{access} {field.FieldType.Name} {field.Name}");
+
+            }
+        }
+    }
+}
