5726432fbafd627955007fcb4ce7b8084a570cb6
Nobody <nobody133738@gmail.com>
2021-09-02 20:50:21 +0900

    Nullable

---
+++ Nullable(널러블)/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Nullable_널러블_
+{
+    class Program
+    {
+        static int Find()
+        {
+            return 0;
+        }
+
+        class Monster
+        {
+            public int Id { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            // Nullable -> Null+  able
+            // Nullable -> 형식?
+            // null
+            // ??
+            // ?.
+
+            int? number = 5;
+
+            if (number != null)
+            {
+                int a = number.Value;
+                Console.WriteLine(a);
+            }
+
+            if (number.HasValue)
+            {
+                int a = number.Value;
+                Console.WriteLine(a);
+            }
+
+            int b = number ?? 0;
+            int c = (number != null) ? number.Value : 0;
+
+
+            Monster monster = null;
+
+            if (monster != null)
+            {
+                int monsterId = monster.Id;
+            }
+
+            int? id = monster?.Id;
+            
+            if (monster == null)
+            {
+                id = null; 
+            }
+            else
+            {
+                id = monster.Id;
+            }
+                      
+        }
+    }
+}
