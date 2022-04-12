0f6309b1fd802902049cac3c567f486f64b7d2a1
DESKTOP-NK217EO\Melchizedek <nobody133738@gmail.com>
2022-04-12 22:20:26 +0900

    Delegate 간단한 예제

---
+++ Delegate_exercise01/Delegate_exercise01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Delegate_exercise01
+{
+    delegate int MyDelegate(int a, int b);    // delegate 선언
+
+    class Calculator
+    {
+        public int Plus(int a, int b)         // delegate는 인스터스 메소드를 참조할수 있다.
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b) // delegate는 정적 메소드도 참조할수 있다.
+        {
+            return a - b;
+        }
+
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // delegate 간단한 예제
+
+            Calculator calculator = new();
+            MyDelegate Callback;
+
+            Callback = new MyDelegate(calculator.Plus);
+            Console.WriteLine(Callback(3, 4));
+                      
+        }
+    }
+}
